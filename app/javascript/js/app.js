34215fbfd8125e9263d6734c0f84c643c2cc58b0
Jhonatan Atuesta <jdatuesta7@gmail.com>
2021-06-09 16:55:06 -0500

    fixed btn collapse

--- app/javascript/js/app.js
+++ app/javascript/js/app.js
@@ -1,5 +1,14 @@
-// $(document).ready(function() {
-//     $('#sidebarCollapse').on('click', function() {
-//         $('#sidebar').toggleClass('active');
-//     });
-// });
+document.addEventListener("DOMContentLoaded", () => {
+    const sideBarCollapse = document.getElementById("sidebarCollapse")
+    const sidebar = document.getElementById("sidebar")
+
+    sideBarCollapse.addEventListener('click', () => {
+        const sidebarAction = sidebar.classList.contains("d-none")
+        
+        if (sidebarAction) {
+            sidebar.classList.remove("d-none")
+        } else {
+            sidebar.classList.add("d-none")
+        }
+    })
+})
